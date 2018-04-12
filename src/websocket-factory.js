f57801ab1010338aa521b6d65b316b2143310b27
github1 <john.collier4@gmail.com>
2018-04-11 23:48:40 -0400

    use https-proxy-agent if HTTP_PROXY is set

--- src/websocket-factory.js
+++ src/websocket-factory.js
@@ -1,9 +1,11 @@
 const Html5WebSocket = require('html5-websocket');
 const ReconnectingWebSocket = require('reconnecting-websocket');
+const HttpsProxyAgent = require('https-proxy-agent');
+const protocols = process.env.HTTP_PROXY ? {agent: new HttpsProxyAgent(process.env.HTTP_PROXY)} : undefined;
 
 module.exports = (address, handler) => {
     address = address.replace(/^http/, 'ws');
-    const rws = new ReconnectingWebSocket(address, undefined, {constructor: Html5WebSocket});
+    const rws = new ReconnectingWebSocket(address, protocols, {constructor: Html5WebSocket});
     rws.addEventListener('message', data => {
         handler(JSON.parse(data.data));
     });
